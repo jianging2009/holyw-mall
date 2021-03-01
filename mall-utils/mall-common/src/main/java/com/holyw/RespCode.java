a354d3fc6c0a8dccab2d4e226a12edb6e90f83bb
DenchyChiang <dengqing.jiang@drgou.com>
2021-03-01 16:53:10 +0800

    first commit

---
+++ mall-utils/mall-common/src/main/java/com/holyw/RespCode.java
@@ -0,0 +1,10 @@
+package com.holyw;
+
+/**
+ * @className: resp
+ * @description: TODO
+ * @author: Denchy Chiang
+ * @date: 2021/3/1
+ **/
+public class RespCode {
+}
---
+++ mall-utils/mall-common/src/main/java/com/holyw/Result.java
@@ -0,0 +1,10 @@
+package com.holyw;
+
+/**
+ * @className: Result
+ * @description: TODO
+ * @author: Denchy Chiang
+ * @date: 2021/3/1
+ **/
+public class Result {
+}
